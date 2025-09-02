c34372e9fcb3c4420940645f0dff2a567913cd60
xuebin <xuebin7@163.com>
2025-09-02 18:03:28 +0800

    更新2025-09-02数据

--- index.js
+++ index.js
@@ -2592,6 +2592,7 @@ let statData = [
   { "date": "2025-08-28", "bondR": 1.85, "hs300V": 4463.78, "hs300PE": 14.10 },
   { "date": "2025-08-29", "bondR": 1.84, "hs300V": 4496.76, "hs300PE": 14.16 },
   { "date": "2025-09-01", "bondR": 1.83, "hs300V": 4523.71, "hs300PE": 14.15 },
+  { "date": "2025-09-02", "bondR": 1.82, "hs300V": 4490.45, "hs300PE": 14.15 },
 ];
 
 const chartDom = document.getElementById("chart");
